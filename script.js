bb82503129cc28a29d30c9b0927bf6ea143342ae
Garegin2003 <garehin.hakobyan.s@gmail.com>
2023-06-30 12:21:41 +0300

    created win games and lose games

--- script.js
+++ script.js
@@ -11,12 +11,16 @@ const ball = {
   x: canvas.width / 2,
   y: canvas.height - platformHeight - platformY - 15,
   radius: 15,
-  color: 'blue',
-  delta: 30
+  color: 'green',
+  delta: 7
 };
+const life = document.querySelector('.life')
+const win = document.querySelector('.win')
+const tryAgain = document.querySelector('.game-over')
 let ballRandom = Math.trunc(Math.random() * 2) - 1;
 let platformX = (canvas.width - platformWidth) / 2;
-let isPressed = false
+let isPressed = false;
+let a = 3
 
 const randomWidths = [];
 for (let j = 0; j < brickRowCount; j++) {
@@ -59,6 +63,12 @@ function drawPlatform() {
 function drawBall() {
   ctx.beginPath()
   ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+  if (a === 2) {
+    ball.color = 'yellow'
+  }
+  if (a === 1) {
+    ball.color = 'red'
+  }
   ctx.fillStyle = ball.color;
   ctx.fill();
   ctx.stroke
@@ -71,6 +81,7 @@ function draw() {
   drawBricks();
   drawBall()
   drawPlatform()
+  winGame()
 }
 function jumpBall() {
   if (ball.y + ball.radius >= canvas.height) {
@@ -81,27 +92,47 @@ function jumpBall() {
   if (!ballRandom) (ballRandom = Math.round(Math.random() * 2) - 1);
   ctx.beginPath();
   ball.y -= ball.delta;
-  ball.x += ballRandom * 30;
+  ball.x += ballRandom * 7;
   ctx.stroke();
-
-
 }
-
-
-
 function checkCollision() {
   for (let i = 0; i < bricks.length; i++) {
     const b = bricks[i];
-    if (ball.x + ball.radius > b.x && ball.x - ball.radius < b.x + b.width && ball.y + ball.radius > b.y && ball.y - ball.radius < b.y + b.height) {
-      ball.delta = -ball.delta
-      bricks.splice(i, 1);
+    if (
+      ball.x + ball.radius > b.x &&
+      ball.x - ball.radius < b.x + b.width &&
+      ball.y + ball.radius > b.y &&
+      ball.y - ball.radius < b.y + b.height
+    ) {
+  
+      bricks.splice(i, 1); 
+      let ballTop = ball.y - ball.radius;
+      let ballBottom = ball.y + ball.radius;
+      let ballLeft = ball.x - ball.radius;
+      let ballRight = ball.x + ball.radius;
+
+      let collisionTop = ballBottom > b.y && ballTop < b.y;
+      let collisionBottom = ballTop < b.y + b.height && ballBottom > b.y + b.height;
+      let collisionLeft = ballRight > b.x && ballLeft < b.x;
+      let collisionRight = ballLeft < b.x + b.width && ballRight > b.x + b.width;
+
+      if ((collisionTop || collisionBottom) && !collisionLeft && !collisionRight) {
+        ball.delta = -ball.delta; 
+      } else if (!collisionTop && !collisionBottom && (collisionLeft || collisionRight)) {
+        ballRandom *= -1; 
+      } else {
+        ball.delta = -ball.delta; 
+        ballRandom *= -1; 
+      }
+      
       break;
     }
-
   }
+
   if (ball.x + ball.radius > canvas.width - ball.delta || ball.x - ball.radius < ball.delta) {
     ballRandom *= -1; 
   }
+
   if (
     ball.y + ball.radius >= canvas.height - platformHeight - platformY &&
     ball.x + ball.radius >= platformX &&
@@ -111,17 +142,17 @@ function checkCollision() {
     ball.y = canvas.height - platformHeight - platformY - ball.radius;
   }
 
-  if (ball.y - ball.radius <=0) {
-    ball.delta = -ball.delta
+  if (ball.y - ball.radius <= 0) {
+    ball.delta = -ball.delta; 
   }
+}
+
+
+
+function winGame() {
 
 }
 
-function gameOver() {
-  if (ball.y === canvas.height) {
-    alert('hajox ape')
-  }
-}
 
 document.addEventListener('keydown', (e) => {
   console.log(e.key);
@@ -139,10 +170,23 @@ document.addEventListener('keydown', (e) => {
   }
 })
 
+function tryAgainHandler() {
+  location.reload()
+}
+
 function loop() {
-  // if (ball.y + ball.radius >= canvas.height) {
-  //   return
-  // }
+  life.innerHTML = `<h1 class = "life__count">${a}</h1>`
+  if (ball.y + ball.radius >= canvas.height) {
+    if (a === 1) {
+      tryAgain.innerHTML = `<h1 onclick = "tryAgainHandler()">tryAgain </h1>`
+      return
+    }
+    else {
+      a--
+
+    }
+  }
+
   requestAnimationFrame(loop)
   draw()
   if (isPressed === true) {
@@ -150,7 +194,11 @@ function loop() {
   }
 
 
-  checkCollision()
+  checkCollision()  
+  if (bricks.length ==0) {
+    win.innerHTML = '<h1 class="win__text">Uraaa</h1> '
+    return
+  }
 }
 
 loop();
