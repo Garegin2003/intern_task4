cd306e08ac3203bd044738719f1c06fbcd863066
Garegin2003 <garehin.hakobyan.s@gmail.com>
2023-07-01 14:13:54 +0300

    fixed all game problems

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const canvas = document.querySelector('#myCanvas');
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 
 const brickRowCount = Math.floor(Math.random() * 6) + 3;
 const brickHeight = 60;
 const bricks = [];
 const platformWidth = 150;
 const platformHeight = 20;
-const platformY = 15
+const platformY = 15;
+const ballRadius = 15;
 const ball = {
   x: canvas.width / 2,
-  y: canvas.height - platformHeight - platformY - 15,
-  radius: 15,
+  y: canvas.height - platformHeight - platformY - ballRadius,
+  radius: ballRadius,
   color: 'green',
-  delta: 7
+  delta: 5,
 };
-const life = document.querySelector('.life')
-const win = document.querySelector('.win')
-const tryAgain = document.querySelector('.game-over')
+const life = document.querySelector('.life');
+const win = document.querySelector('.win');
+const tryAgain = document.querySelector('.game-over');
 let ballRandom = Math.trunc(Math.random() * 2) - 1;
 let platformX = (canvas.width - platformWidth) / 2;
 let isPressed = false;
-let a = 3
+let a = 3;
 
 const randomWidths = [];
 for (let j = 0; j < brickRowCount; j++) {
@@ -39,13 +40,14 @@ for (let i = 0; i < brickRowCount; i++) {
       y: brickY,
       width: brickWidth,
       height: brickHeight,
-      color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+      color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
+        Math.random() * 255
+      })`,
     });
     brickX += brickWidth;
   }
   brickY += brickHeight;
 }
-
 
 function drawBricks() {
   for (let i = 0; i < bricks.length; i++) {
@@ -55,33 +57,35 @@ function drawBricks() {
 }
 
 function drawPlatform() {
-  ctx.fillStyle = "red";
-  ctx.fillRect(platformX, canvas.height - platformHeight - platformY, platformWidth, platformHeight);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(
+    platformX,
+    canvas.height - platformHeight - platformY,
+    platformWidth,
+    platformHeight
+  );
 }
 
-
 function drawBall() {
-  ctx.beginPath()
+  ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
   if (a === 2) {
-    ball.color = 'yellow'
+    ball.color = 'yellow';
   }
   if (a === 1) {
-    ball.color = 'red'
+    ball.color = 'red';
   }
   ctx.fillStyle = ball.color;
   ctx.fill();
-  ctx.stroke
+  ctx.stroke();
 }
-
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   drawBricks();
-  drawBall()
-  drawPlatform()
-  winGame()
+  drawBall();
+  drawPlatform();
 }
 function jumpBall() {
   if (ball.y + ball.radius >= canvas.height) {
@@ -89,10 +93,10 @@ function jumpBall() {
     ball.delta = -ball.delta;
   }
 
-  if (!ballRandom) (ballRandom = Math.round(Math.random() * 2) - 1);
+  if (!ballRandom) ballRandom = Math.round(Math.random() * 2) - 1;
   ctx.beginPath();
   ball.y -= ball.delta;
-  ball.x += ballRandom * 7;
+  ball.x += ballRandom * 5;
   ctx.stroke();
 }
 function checkCollision() {
@@ -104,101 +108,95 @@ function checkCollision() {
       ball.y + ball.radius > b.y &&
       ball.y - ball.radius < b.y + b.height
     ) {
-  
-      bricks.splice(i, 1); 
-      let ballTop = ball.y - ball.radius;
-      let ballBottom = ball.y + ball.radius;
-      let ballLeft = ball.x - ball.radius;
-      let ballRight = ball.x + ball.radius;
+      bricks.splice(i, 1);
 
-      let collisionTop = ballBottom > b.y && ballTop < b.y;
-      let collisionBottom = ballTop < b.y + b.height && ballBottom > b.y + b.height;
-      let collisionLeft = ballRight > b.x && ballLeft < b.x;
-      let collisionRight = ballLeft < b.x + b.width && ballRight > b.x + b.width;
+      let closestX = Math.max(b.x, Math.min(ball.x, b.x + b.width));
+      let closestY = Math.max(b.y, Math.min(ball.y, b.y + b.height));
 
-      if ((collisionTop || collisionBottom) && !collisionLeft && !collisionRight) {
-        ball.delta = -ball.delta; 
-      } else if (!collisionTop && !collisionBottom && (collisionLeft || collisionRight)) {
-        ballRandom *= -1; 
-      } else {
-        ball.delta = -ball.delta; 
-        ballRandom *= -1; 
+      let distanceX = ball.x - closestX;
+      let distanceY = ball.y - closestY;
+      let distanceSquared =
+        (distanceX / ball.radius) * (distanceX / ball.radius) +
+        (distanceY / ball.radius) * (distanceY / ball.radius);
+
+      if (distanceSquared <= ball.radius * ball.radius) {
+        let angle = Math.atan2(distanceY, distanceX);
+        let collisionAngle = Math.abs(angle);
+
+        if (
+          collisionAngle > Math.PI / 4 &&
+          collisionAngle < (3 * Math.PI) / 4
+        ) {
+          ball.delta *= -1;
+        } else {
+          ballRandom *= -1;
+        }
       }
-      
+
       break;
     }
   }
-
-  if (ball.x + ball.radius > canvas.width - ball.delta || ball.x - ball.radius < ball.delta) {
-    ballRandom *= -1; 
+  if (ball.x + ball.radius >= canvas.width || ball.x - ball.radius <= 0) {
+    ballRandom *= -1;
   }
 
   if (
     ball.y + ball.radius >= canvas.height - platformHeight - platformY &&
-    ball.x + ball.radius >= platformX &&
-    ball.x - ball.radius <= platformX + platformWidth
+    ball.y + ball.radius <= canvas.height - platformY &&
+    ball.x >= platformX &&
+    ball.x <= platformX + platformWidth
   ) {
     ball.delta = -ball.delta;
     ball.y = canvas.height - platformHeight - platformY - ball.radius;
   }
-
   if (ball.y - ball.radius <= 0) {
-    ball.delta = -ball.delta; 
+    ball.delta = -ball.delta;
+    ball.y = ball.radius;
   }
 }
-
-
-
-function winGame() {
-
-}
-
 
 document.addEventListener('keydown', (e) => {
   console.log(e.key);
   if (e.key === 'ArrowRight' || e.key === 'd') {
-    (platformX <= (canvas.width - platformWidth)) && (platformX += 20)
-    !isPressed && ball.x <= canvas.width - ball.radius - platformWidth / 2 && (ball.x += 20)
+    platformX <= canvas.width - platformWidth && (platformX += 20);
+    !isPressed &&
+      ball.x <= canvas.width - ball.radius - platformWidth / 2 &&
+      (ball.x += 20);
   }
   if (e.key === 'ArrowLeft' || e.key === 'a') {
-    (platformX >= 0) && (platformX -= 20)
-    !isPressed && ball.x >= platformWidth / 2 && (ball.x -= 20)
-
+    platformX >= 0 && (platformX -= 20);
+    !isPressed && ball.x >= platformWidth / 2 && (ball.x -= 20);
   }
   if (e.key === ' ') {
-    isPressed = true
+    isPressed = true;
   }
-})
+});
 
 function tryAgainHandler() {
-  location.reload()
+  location.reload();
 }
 
 function loop() {
-  life.innerHTML = `<h1 class = "life__count">${a}</h1>`
+  life.innerHTML = `<h1 class = "life__count">${a}</h1>`;
   if (ball.y + ball.radius >= canvas.height) {
     if (a === 1) {
-      tryAgain.innerHTML = `<h1 onclick = "tryAgainHandler()">tryAgain </h1>`
-      return
-    }
-    else {
-      a--
-
+      tryAgain.innerHTML = `<h1 onclick = "tryAgainHandler()">tryAgain </h1>`;
+      return;
+    } else {
+      a--;
     }
   }
-
-  requestAnimationFrame(loop)
-  draw()
+  if (bricks.length === 0) {
+    win.innerHTML = '<h1 class="win__text">Uraaa</h1> ';
+    return;
+  }
+  requestAnimationFrame(loop);
+  draw();
   if (isPressed === true) {
-    jumpBall()
+    jumpBall();
   }
 
-
-  checkCollision()  
-  if (bricks.length ==0) {
-    win.innerHTML = '<h1 class="win__text">Uraaa</h1> '
-    return
-  }
+  checkCollision();
 }
 
 loop();
